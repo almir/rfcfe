86808255f91bd5b185f8d81d7653cca6fea02745
almir <almir@506a0155-a2ad-4509-921c-63e09356eb98>
2011-04-29 07:00:07 +0000

    Aesthetic changes in the Credits dialog.
    git-svn-id: https://192.168.2.202:8333/svn/rfcfe@16 506a0155-a2ad-4509-921c-63e09356eb98

--- RFCFE/RFCFE/CreditsDialog.Designer.cs
+++ RFCFE/RFCFE/CreditsDialog.Designer.cs
@@ -45,40 +45,41 @@ namespace RFCFE
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CreditsDialog));
-            this.creditsTxtBox = new System.Windows.Forms.RichTextBox();
             this.CrdDlgClsBtn = new System.Windows.Forms.Button();
+            this.textBoxCredits = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // creditsTxtBox
-            // 
-            this.creditsTxtBox.BackColor = System.Drawing.SystemColors.Window;
-            this.creditsTxtBox.Dock = System.Windows.Forms.DockStyle.Top;
-            this.creditsTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.creditsTxtBox.Location = new System.Drawing.Point(0, 0);
-            this.creditsTxtBox.Name = "creditsTxtBox";
-            this.creditsTxtBox.ReadOnly = true;
-            this.creditsTxtBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
-            this.creditsTxtBox.Size = new System.Drawing.Size(432, 243);
-            this.creditsTxtBox.TabIndex = 0;
-            this.creditsTxtBox.Text = resources.GetString("creditsTxtBox.Text");
             // 
             // CrdDlgClsBtn
             // 
-            this.CrdDlgClsBtn.Location = new System.Drawing.Point(168, 249);
+            this.CrdDlgClsBtn.Location = new System.Drawing.Point(112, 184);
             this.CrdDlgClsBtn.Name = "CrdDlgClsBtn";
-            this.CrdDlgClsBtn.Size = new System.Drawing.Size(75, 23);
+            this.CrdDlgClsBtn.Size = new System.Drawing.Size(160, 32);
             this.CrdDlgClsBtn.TabIndex = 1;
             this.CrdDlgClsBtn.Text = "&Close";
             this.CrdDlgClsBtn.UseVisualStyleBackColor = true;
             this.CrdDlgClsBtn.Click += new System.EventHandler(this.CrdDlgClsBtn_Click);
             // 
+            // textBoxCredits
+            // 
+            this.textBoxCredits.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxCredits.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBoxCredits.Location = new System.Drawing.Point(0, 0);
+            this.textBoxCredits.Margin = new System.Windows.Forms.Padding(6, 3, 3, 3);
+            this.textBoxCredits.Multiline = true;
+            this.textBoxCredits.Name = "textBoxCredits";
+            this.textBoxCredits.ReadOnly = true;
+            this.textBoxCredits.Size = new System.Drawing.Size(378, 178);
+            this.textBoxCredits.TabIndex = 24;
+            this.textBoxCredits.TabStop = false;
+            this.textBoxCredits.Text = resources.GetString("textBoxCredits.Text");
+            // 
             // CreditsDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(432, 275);
+            this.ClientSize = new System.Drawing.Size(378, 221);
+            this.Controls.Add(this.textBoxCredits);
             this.Controls.Add(this.CrdDlgClsBtn);
-            this.Controls.Add(this.creditsTxtBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -89,12 +90,13 @@ namespace RFCFE
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Credits";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.RichTextBox creditsTxtBox;
         private System.Windows.Forms.Button CrdDlgClsBtn;
+        private System.Windows.Forms.TextBox textBoxCredits;
     }
 }
